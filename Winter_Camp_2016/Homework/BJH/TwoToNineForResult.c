4f2e3b39f0c57008d424be37bf195b7664da1602
Manicarus <swkang7229@gmail.com>
2016-01-06 11:30:03 +0900

    Update TwoToNineForResult.c
    코멘트 추가

--- Winter_Camp_2016/Homework/BJH/TwoToNineForResult.c
+++ Winter_Camp_2016/Homework/BJH/TwoToNineForResult.c
@@ -2,13 +2,16 @@
 
 int main()
 {
-	int a, b;
-	
-	for(a=2; a<10; a++)
-	{
-		for(b=1; b<10; b++)
-			printf("%d x %d = %d \n", a, b, a*b);
-		printf("\n");
-	}
-	return 0;
+    /* 코멘트 2016_01_06
+    for문이 아니라 while문을 사용해서 다시 작성해주세요
+    그리고 들여쓰기를 할 때, tab보다는 space키 4번을 사용해주세요
+    */
+    int a, b;
+    for(a=2; a<10; a++)
+    {
+        for(b=1; b<10; b++)
+        printf("%d x %d = %d \n", a, b, a*b);
+        printf("\n");
+    }
+    return 0;
 }
