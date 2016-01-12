2ee6d7d621dea5e1abf7df6e9a0edf56cd0b1487
Manicarus <swkang7229@gmail.com>
2016-01-12 13:06:10 +0900

    TwoToNineForResult.c 이동

--- Mentoring/Winter_Camp_2016/Homework/BJH/20160105/TwoToNineForResult.c
+++ Mentoring/Winter_Camp_2016/Homework/BJH/20160105/TwoToNineForResult.c
@@ -2,16 +2,19 @@
 
 int main()
 {
-    /* 코멘트 2016_01_06
-    for문이 아니라 while문을 사용해서 다시 작성해주세요
-    그리고 들여쓰기를 할 때, tab보다는 space키 4번을 사용해주세요
-    */
-    int a, b;
-    for(a=2; a<10; a++)
-    {
-        for(b=1; b<10; b++)
-        printf("%d x %d = %d \n", a, b, a*b);
-        printf("\n");
-    }
-    return 0;
+    int a=2;
+    int b=0;
+	
+	while(a<10)
+	{
+		b=1;
+		while(b<10)
+		{
+			printf("%d x %d = %d \n", a, b, a*b);
+			b++;
+		}
+		printf("\n");
+		a++;
+	}
+	return 0;
 }
